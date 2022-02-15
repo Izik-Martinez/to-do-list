46112e923b38396f1f79fcff5c32bbab23cfbef8
Izik-Martinez <izikma17@yahoo.com>
2022-02-15 09:03:19 -0500

    commented in 'client' guidelines to work within

---
+++ script.js
@@ -0,0 +1,12 @@
+// Everything is done via prompt()
+// First user-step is to enter a command
+// When user adds something to the list, console.log("userInput" added to list)
+//    and it takes user back to originial prompt box for a command
+
+// List command console.log()'s all to-do's on the list with an index beside of it
+// 'Delete' command prompts user to enter the index of the item they want to delete
+// After entering the index to remove, console.log() "To-do removed"
+
+// If user enters an invalid command/value, just keep looping the question
+// "quit" will quit the 'app' and console.log() a quit-confirmation message
+
